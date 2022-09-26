c5822c4d1a274863a2f21311d54ee9830a51f5f9
Anish168 <87292453+Anish168@users.noreply.github.com>
2022-09-26 22:40:20 +0530

    Add files via upload

---
+++ Web Development/JavaScript/script2.js
@@ -0,0 +1,33 @@
+// var outerdiv = document.getElementById("outer");
+// outerdiv.addEventListener('mouseover', function () {
+//     console.log("Mouse Over");
+// });
+// outerdiv.addEventListener('mouseout', function () {
+//     console.log("Mouse Out");
+// });
+// outerdiv.addEventListener('mouseenter', function () {
+//     console.log("Mouse Enter");
+// });
+
+// var button = document.getElementById('search');
+// button.onmouseenter = function (event) {
+//  console.log("Mouse entered element - ");
+// }
+
+// var searchInput=document.getElementById('search');
+// searchInput.addEventListener('keypress',function(){
+//     console.log("Key is pressed");
+// })
+
+// document.addEventListener('keypress',function(){
+//     console.log("Key is pressed");
+// });
+// document.addEventListener('keydown',function(){
+//     console.log("Keydown is pressed");
+// })
+// document.addEventListener('keyup',function(){
+//     console.log("Keyup is pressed");
+// })
+// document.addEventListener('keydown',function(event){
+//     console.log("Keydown is pressed",event.keyCode);
+// })
